e94fd2fa3cdab02da9d64ec632d9d4b008ab27ef
Raman <241ramankumar@gmail.com>
2026-04-01 12:56:15 +0530

    done somethin

---
+++ Server/final Server/app/routers/user_router.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter,Depends
+from app.schemas.user import UserCreate
+from sqlalchemy.orm import Session
+from app.dependencies import get_db
+router=APIRouter(prefix="/users",tags=["users"])
+
+@router.post("/",response_model=UserCreate)
+def create_new_user(user:UserCreate,db:Session=Depends(get_db)):
+    return create_user(db,user)
